04167ea62d341293d2c433f00996369af533c432
Nikita Gupta <gnikita432@gmail.com>
2021-07-02 19:35:06 +0530

    Added functionality in skills section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ------------Navbar-----------------
+
 /* MENU SHOW Y HIDDEN */
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
@@ -26,3 +28,24 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// ---------------Skills--------------------
+
+/* ACCORDION SKILLS */
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
