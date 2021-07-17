15a1deafce40728c260d76f978125ed6a9316d89
Nikita Gupta <gnikita432@gmail.com>
2021-07-17 17:24:41 +0530

    Implemented Scroll sections active link functionality

--- script.js
+++ script.js
@@ -86,3 +86,24 @@ let swiper = new Swiper('.portfolio__container', {
     mousewheel: true,
     keyboard: true,
   });
+
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
