dfe799c0be72c1eb2712c4e848658a8c40e85a06
Nikita Gupta <gnikita432@gmail.com>
2021-12-08 16:31:20 +0530

    Change background header

--- script.js
+++ script.js
@@ -135,3 +135,12 @@ $(document).ready(function () {
     }
   });
 });
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
